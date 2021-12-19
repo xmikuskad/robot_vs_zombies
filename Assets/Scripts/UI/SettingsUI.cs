871ab33aa6b32748b6bbd54e10c1b7eff3f89ab9
Dominik Mikuška <43302506+xmikuskad@users.noreply.github.com>
2021-12-19 19:19:14 +0100

    added main menu and settings design

--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SettingsUI : MySceneManager
+public class SettingsUI : MainMenu
 {
     [SerializeField]
     private float defaultGlobalVolume = 0.5f;
